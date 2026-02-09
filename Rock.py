815bc3c46539838cee1623f1c1531d4c73895487
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-02-09 14:14:05 +0100

    rajout de la class Rock

---
+++ Rock.py
@@ -0,0 +1,7 @@
+class Rock:
+    def __init__ (self):
+        self.weaknesses=["Water","Grass","Fighting","Ground","Steel"]
+        self.resistances=["Normal","Fire","Poison","Flying"]
+        self.immunities=[]
+        
+        
