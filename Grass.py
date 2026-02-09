08fe7975bd705c036fa6e58177ac34d1b7bea392
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-02-09 13:25:38 +0100

    rajout de la class grass

---
+++ Grass.py
@@ -0,0 +1,5 @@
+class Grass:
+    def __init__ (self):
+        self.weaknesses=["Fire","Ice","Poison","Flying","Bug"]
+        self.resistances=["Water","Electric","Grass","Ground"]
+        self.immunities=[]
