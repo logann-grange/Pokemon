b2d8c0c1a964c7b506079e8c9380f8dc5aadcd9f
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-02-09 14:19:54 +0100

    rajout de la class Steel

---
+++ Steel.py
@@ -0,0 +1,5 @@
+class Steel:
+    def __init__ (self):
+        self.weaknesses=["Fire","Fighting","Ground"]
+        self.resistances=["Normal","Grass","Ice","Flying","Psychic","Bug","Rock","Dragon","Steel","Fairy"]
+        self.immunities=["Poison"]
