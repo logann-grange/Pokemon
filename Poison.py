d369b9ef4413de445337a7fce6dcae729341bcc8
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-02-09 14:26:51 +0100

    rajout de la class Poison

---
+++ Poison.py
@@ -0,0 +1,5 @@
+class Poison:
+    def __init__ (self):
+        self.weaknesses=["Ground","Psychic"]
+        self.resistances=["Grass","Fighting","Poison","Bug","Fairy"]
+        self.immunities=[]
