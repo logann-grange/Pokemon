9607df7da5d7713ab743ccc7f602bbbbe42448e1
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-02-09 13:17:57 +0100

    création de la class feu

---
+++ Feu.py
@@ -0,0 +1,7 @@
+class Fire:
+    def __init__ (self):
+        self.weaknesses=["Water","Ground","Rock"]
+        self.resistances=["Fire","Grass","Ice","Bug","Steel","Fairy"]
+        self.immunities=[]
+        
+        
