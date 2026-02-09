bcb8aa8cd16fd9c497c77b4f6188ec7706284929
logann-grange <logann.grange@laplateforme.io>
2026-02-09 12:56:31 +0100

    creation de la classe Pokemon

---
+++ Pokemon.py
@@ -0,0 +1,17 @@
+class Pokemon() :
+
+    def __init__(self, id, name, image, coord, hp, attack, defense, level, xp) :
+        self.id = id
+        self.name = name
+        self.image = image
+        self.coord = coord
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.level = level
+        self.xp = xp
+
+    def level_up(self) :
+        if self.xp <= 1000 * self.level*0.5 : #multiplier par un facteur d'xp
+            self.level += 1
+            self.xp -=  1000 * self.level
